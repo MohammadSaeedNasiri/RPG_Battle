3822e53f52e027e29893617fc4b6ecab9327e053
Mohammad Saeed Nasiri <130396754+MohammadSaeedNasiri@users.noreply.github.com>
2026-01-21 01:53:56 +0330

    refactor(HeroPicker ): improve HeroPicker

--- Assets/Scripts/Heros/Pick/HeroPicker.cs
+++ Assets/Scripts/Heros/Pick/HeroPicker.cs
@@ -2,28 +2,27 @@ using UnityEngine;
 
 public class HeroPicker : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerDeckManager playerDeckManager;
-    [SerializeField]
-    private HeroItemsManager heroItemsManager;
-    [SerializeField]
-    private HeroItemsLoader heroItemsLoader;
-    [SerializeField]
-    private GameObject selectHeroPage;
+    [SerializeField] private PlayerDeckManager playerDeckManager;
+    [SerializeField] private HeroItemsManager heroItemsManager;
+    [SerializeField] private HeroItemsLoader heroItemsLoader;
+    [SerializeField] private GameObject selectHeroPage;
 
     private int selectedSlotIndex;
-    public void OpenHeroMenuForSelectHero(int slotIndex)
+    public void OpenHeroMenuForSelectHero(int slotIndex)// Opens the hero selection menu for a specific deck slot.
     {
         selectedSlotIndex = slotIndex;
-        MenuManager.Instance.OpenMenu(selectHeroPage);
-        heroItemsLoader.ReloadHeroesInUIForPick();
+        MenuManager.Instance.OpenMenu(selectHeroPage);// Open the hero selection UI
+        heroItemsLoader.ReloadHeroesInUIForPick(); // Refresh the hero items display
     }
 
-    public void UseSelectedHeroForPlayerDeck()
+    public void AssignSelectedHeroToDeckSlot()// Assigns the currently selected hero to the previously chosen deck slot.
     {
-        playerDeckManager.SetPlayerDeckCard(selectedSlotIndex, heroItemsManager.GetSelectedHeroItem().GetHeroItemData().id);
-        MenuManager.Instance.CloseLastMenu();
-        playerDeckManager.ReloadPlayerDeckCard(selectedSlotIndex);
+        var selectedHero = heroItemsManager.GetSelectedHeroItem();
+        if (selectedHero == null) return;// Exit if no hero is selected
+
+        playerDeckManager.SetPlayerDeckCard(selectedSlotIndex, selectedHero.GetHeroItemData().id); // Assign the selected hero to the player's deck slot
+        MenuManager.Instance.CloseLastMenu();// Close the hero selection menu
+        playerDeckManager.ReloadPlayerDeckCard(selectedSlotIndex);// Refresh the deck slot UI
     }
 
 }
