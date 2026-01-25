d1ffaf9ca305f71ea36b0da8128993a22b76129d
Mohammad Saeed Nasiri <130396754+MohammadSaeedNasiri@users.noreply.github.com>
2026-01-25 17:47:51 +0330

    Feat (Damage Anim UI) : Add Damage UI Animation

--- Assets/Scripts/Game Play/View_UI/HeroUI.cs
+++ Assets/Scripts/Game Play/View_UI/HeroUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,12 +7,47 @@ public class HeroUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private Slider healthSlider;
+    [SerializeField] private TextMeshProUGUI damageAnimatedText;
+
 
 
     public void Initialize(HeroRuntimeData hr)
     {
+        healthSlider.maxValue = hr.heroExprienceData.health;
+
+        UpdateUI(hr);
+       if (hr.heroType == HeroType.EnemyHero)
+            GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+    public void Damage(HeroRuntimeData hr,int damageValue)
+    {
+        UpdateUI(hr);
+        ShowDamageAnim(damageValue);
+    }
+
+
+    private void UpdateUI(HeroRuntimeData hr)
+    {
         nameText.text = hr.heroData.heroName;
-        healthSlider.value = healthSlider.maxValue = hr.heroExprienceData.health;
-       
+        healthSlider.value = hr.heroExprienceData.health;
+    }
+
+    private void ShowDamageAnim(int damageValue)
+    {
+        damageAnimatedText.text = damageValue.ToString();
+        StartCoroutine(PlayAnimationCoroutine(damageAnimatedText.GetComponent<Animator>()));
+       // damageAnimatedText.gameObject.SetActive(true);
+    }
+    private IEnumerator PlayAnimationCoroutine(Animator animator)
+    {
+        animator.SetBool("Play", true);
+
+        yield return new WaitForSeconds(0.05f);
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        yield return new WaitForSeconds(stateInfo.length);
+
+        animator.SetBool("Play", false);
     }
 }
