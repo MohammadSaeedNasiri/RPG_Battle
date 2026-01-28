9be3e5e368812c767d49c2db80f1ef7557613b82
Mohammad Saeed Nasiri <130396754+MohammadSaeedNasiri@users.noreply.github.com>
2026-01-29 01:44:07 +0330

    feat(Background music) : Add background music

---
+++ Assets/Scripts/UI/Sound/MusicManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+
+    [SerializeField] private AudioClip backgroundMusic;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+        SetupAudioSource();
+        PlayMusic();
+    }
+
+    private void SetupAudioSource()
+    {
+        audioSource.clip = backgroundMusic;
+        audioSource.loop = true;
+        audioSource.playOnAwake = false;
+        audioSource.volume = 0.6f;
+    }
+
+    private void PlayMusic()
+    {
+        if (!audioSource.isPlaying)
+            audioSource.Play();
+    }
+}
